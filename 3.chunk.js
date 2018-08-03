003507a3102cf66f6691a27796e6d1aa87c683ed
Dominik Moritz (from Travis CI) <domoritz@gmail.com>
2018-08-03 04:47:27 +0000

    Deploy editor to github.com/vega/editor.git:gh-pages

--- 3.chunk.js
+++ 3.chunk.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{810:function(n,o,r){var e={"vs/language/json/jsonWorker":809};function t(n){var o=s(n);return r(o)}function s(n){var o=e[n];if(!(o+1)){var r=new Error("Cannot find module '"+n+"'");throw r.code="MODULE_NOT_FOUND",r}return o}t.keys=function(){return Object.keys(e)},t.resolve=s,n.exports=t,t.id=810}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{940:function(n,o,r){var e={"vs/language/json/jsonWorker":939};function t(n){var o=s(n);return r(o)}function s(n){var o=e[n];if(!(o+1)){var r=new Error("Cannot find module '"+n+"'");throw r.code="MODULE_NOT_FOUND",r}return o}t.keys=function(){return Object.keys(e)},t.resolve=s,n.exports=t,t.id=940}}]);
 //# sourceMappingURL=3.chunk.js.map
